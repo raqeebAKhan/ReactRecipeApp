af7b256b71e6be884f5e5174330edec903f7ae46
raqeebAKhan <ritzyraqeeb@gmail.com>
2022-08-19 22:10:21 +0530

    Receipe code done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import Recipe from "./Recipe";
+import { FaUtensils } from "react-icons/fa";
+
+
 
 function App() {
+  const [recipes, setrecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
+  
+  
+
+ const APP_ID = "82e86bf0";
+ const APP_KEY = "446705026b6606983607169d0a6672fe";
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  async function getRecipes() {
+    const result = await fetch(url);
+    const data = await result.json();
+    setrecipes(data.hits);
+  }
+
+  useEffect(() => {
+     getRecipes();
+  }, [query]);
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+      <h1 style={{color: "purple"}}><FaUtensils/>RAK</h1>
+      <h3 style={{color: "purple"}} >Recipes</h3>
+      <form onSubmit={getSearch} className="search_box">
+        <input type="text" className="app_input" placeholder="Search Item" value={search} onChange={updateSearch}/>
+
+        <input className="app_submit" type="submit" value="Search"></input>
+        
+      </form>
+
+      <div className='recepies'>
+        {recipes.map(recipe => (
+          <Recipe 
+          key={recipe.recipe.label}
+          image={recipe.recipe.image}
+          title={recipe.recipe.label}
+          time={recipe.recipe.totalTime}
+          url={recipe.recipe.url}
+          cuisineType={recipe.recipe.cuisineType}
+        />
+        ))}
+      </div>
+      </div>
   );
 }
 
